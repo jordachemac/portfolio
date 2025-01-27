00bc7b4fa6b1216a497d2cfd5edfdf9bf919bd97
Huxston <bitbucket@jordachemac.com>
2025-01-27 23:22:39 +1300

    fix capitalisation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import styles from './Footer.module.scss';
+import styles from './footer.module.scss';
 
 const Footer: React.FC = () => {
     const { t } = useTranslation();
