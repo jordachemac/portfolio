83bf6dadd9baface2e94f9768e9986f1d8e50b8e
Huxston <bitbucket@jordachemac.com>
2025-02-04 21:23:32 +1300

    initial commit

--- src/components/HeroSection/heroSection.tsx
+++ src/components/HeroSection/heroSection.tsx
@@ -8,20 +8,10 @@ const HeroSection: React.FC = () => {
     return (
         <div className={styles.root}>
             <header className={styles.title}>
-                <h1>{t('hero.title', { company: 'NZ Organics Group' })}</h1>
+                <h1>{t('hero.title')}</h1>
             </header>
             <section className={styles.paragraph}>
-                <p className={styles.text}>
-                    <Trans i18nKey="hero.description">
-                        NZ Organics Group Limited is dedicated to building a leading New
-                        Zealand-based export and production business, specializing in natural,
-                        organic, and GMO-free products. Our mission is to unite a number of New
-                        Zealand's top food production companies, including{' '}
-                        <strong>Over the Moon Dairy</strong>, <strong>Manuka Health</strong>, and{' '}
-                        <strong>Eggs of NZ</strong>, to create a global powerhouse focused on
-                        premium, organic food products for international markets.
-                    </Trans>
-                </p>
+                <p className={styles.text}></p>
             </section>
         </div>
     );
